6276d2e8993265ce83b187d7ef229e2e7d53dbda
Jason McIver <mciver.jason@gmail.com>
2016-05-01 18:36:56 +1000

    bug

--- client/components/ViewPost.js
+++ client/components/ViewPost.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default React.createClass({
   render() {
-    let post = this.props.posts.filter( (post) => {
-      if(post.id == self.props.params.id) return post
-    })[0]
+    let post = this.props.posts.filter((post) => {
+      if (post.id == this.props.params.id) return post;
+    })[0];
     return (
 			<div>
 				<h3>Post details</h3>
 				<p>{post.title}</p>
 			</div>
-		),
-  };
+		);
+  },
 });
